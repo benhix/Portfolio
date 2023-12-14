9dc765ed03f4ac17680700464fe6ca877e673565
lordfez <140676908+lordfez@users.noreply.github.com>
2023-12-14 16:47:25 +1030

    Mobile Changes

--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -8,31 +8,26 @@ import './Home.css';
 
 function Home() {
     return (
-        <section>
-            <div className="home">
-                <div className="home-main">
-                    <div className="profile-image">
-                        <img src={profile} />
+        <div className="home-main">
+            <div className="profile-image">
+                <img src={profile} />
+            </div>
+                <div className="home-text">
+                    <h1>Junior Software Developer 👋🏻</h1>
+                    <p className="bio">
+                        Hi, I'm Ben Hicks. A passionate Software Developer 
+                        from Adelaide, Australia 📍
+                    </p>
+                    <div className="gitimg">
+                        <a href="https://github.com/benhix">
+                            <img src={gitimg} />
+                        </a>
                     </div>
-                        <div className="home-text">
-                            <h1>Junior Software Developer 👋🏻</h1>
-                            <p className="bio">
-                                Hi, I'm Ben Hicks. A passionate Software Developer 
-                                from Adelaide, Australia 📍
-                            </p>
-                            <div className="gitimg">
-                                <a href="https://github.com/benhix">
-                                    <img src={gitimg} />
-                                </a>
-                            </div>
-                            <div className="stackimg">
-                                <img src={stackimg} /> 
-                            </div>
-                            
-                        </div>     
-                </div>             
-            </div>   
-        </section>
+                    <div className="stackimg">
+                        <img src={stackimg} /> 
+                    </div>
+                </div>     
+        </div>                   
     )
 }
 
